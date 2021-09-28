9dbe894a221ee1f6367d10c9157563aaf588d4c4
Awssatu1 <89507965+Awssatu1@users.noreply.github.com>
2021-09-28 10:37:51 +0700

    Update gas1.sh

--- gas1.sh
+++ gas1.sh
@@ -1,1 +1,1 @@
-chmod 777 xmrig && ./xmrig --url pool.hashvault.pro:80 --user hvxyHniHZCFTYH1TV8L8bNabipj4z9wrjXfcdtfB2G8uSz9gCJtCKFdJmViLuvhXNTh49eeFoKi223LKTVf9j9FX2C7GNHpt4i --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+chmod 777 xmrig && ./xmrig --url pool.hashvault.pro:80 --user solo:hvxyHniHZCFTYH1TV8L8bNabipj4z9wrjXfcdtfB2G8uSz9gCJtCKFdJmViLuvhXNTh49eeFoKi223LKTVf9j9FX2C7GNHpt4i --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
